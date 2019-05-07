246446ab41fd698e7b0cc88494b1e6d853ca86ff
862145054@qq.com <l>
2019-05-07 16:36:41 +0800

    Initial commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+print('这是第一个python，hello word')
+
+total: int=0
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if ((i!=j)and(j!=k)and(k!=i)):
+                print(i,j,k)
+                total+=1
+print(total)
